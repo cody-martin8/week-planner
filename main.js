8a0befb3b3796e1e874b032b1ba45e076b546ee8
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-22 22:00:05 +0000

    saved file

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
 // adding the entries
 //   identify inputs on add entry page
 //   pull data from inputs and add to data.entries as new object
+// time, description, day of the week,
 
 var $addEntryButton = document.querySelector('.button.add-entry');
 
@@ -27,5 +28,8 @@ var $entryForm = document.getElementById('entry-form');
 console.log($entryForm);
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
-
+  var plannerEntry = {};
+  plannerEntry.time =
+  plannerEntry.day =
+  plannerEntry.description =
 });
