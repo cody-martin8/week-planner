ef1e2ce0fea4f8f028d717795e048f24b59c22a3
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-22 20:23:13 +0000

    finished html / css styling for modal and entry

---
+++ main.js
@@ -0,0 +1,7 @@
+// identify button with query selector
+// identify the modal div element
+// identify submit button
+// add event listener for add entry button
+// turn off hidden className
+// add event listener for submit
+// turn on hidden className
