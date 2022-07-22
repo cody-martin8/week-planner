15fc44c33571c9070966fa528f9e2e759af2b752
Cody Martin <12martincody@gmail.com>
2022-07-22 20:33:33 +0000

    JS functionality for buttons.

--- main.js
+++ main.js
@@ -5,3 +5,18 @@
 // turn off hidden className
 // add event listener for submit
 // turn on hidden className
+
+var $addEntryButton = document.querySelector('.button.add-entry');
+console.log($addEntryButton);
+var $submitButton = document.querySelector('.button.submit');
+console.log($submitButton);
+var $modal = document.getElementById('modal');
+console.log($modal);
+
+$addEntryButton.addEventListener('click', function addEntry() {
+  $modal.className = 'row';
+});
+
+$submitButton.addEventListener('click', function submitEntry() {
+  $modal.className = 'row hidden';
+});
